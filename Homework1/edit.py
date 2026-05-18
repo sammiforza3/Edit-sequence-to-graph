9cc5b2ad1d94ffd7cb89d321b59315cc09d99fab
samuele <bergaminsamuele.1@gmail.com>
2026-05-18 19:11:31 +0200

    need to fix matrix generation

--- Homework1/edit.py
+++ Homework1/edit.py
@@ -6,6 +6,8 @@ def predecessore(graph, successore):
         #print(graph[nodo])
         if successore in graph[nodo]:
             pred.append(nodo) 
+    if pred == []:
+        return "0"
     return pred
 
 def instanziaTabella(pattern, graph):
@@ -29,20 +31,105 @@ def ordinaTopo(graph, head):
                 try:
                     dfs(children)
                 except:
-                    print("end of the line :3")
+                    print("")
     dfs(head)
     return topoList
+
+def emptyMatrix(righe, colonne):
+    matrix = []
+
+    for i in range(0,righe):
+        riga = []
+        for j in range(0,colonne):
+            riga.append(0)
+        matrix.append(riga)
+
+    return matrix;
+
+
+
+    
+
+def calcolateMove(matrix, riga, colonna, graph, node, topolist):
+    #troviamo il predecessore con costo minore
+    pred = predecessore(graph, node)
+
+    #match 
+    #troviamo il predecessore che pesa meno SULLA MATRICE alla riga precedente
+    celle = []
+    for i in range (0, colonna):
+        if topolist[i] in pred:
+            celle.append(matrix[riga-1][i])
+    match = min(celle)
+
+    #insert
+    insert = matrix[riga-1][colonna]
+
+    #delete
+    celle = []
+    for i in range (0, colonna):
+        if topolist[i] in pred:
+            celle.append(matrix[riga][i])
+    delete = min(celle)
+
+    return min([match, insert, delete])
 
 
 
 
     
 
+#function that buils the dynamic programming matrix
+def buildMatrix(graph, etichette, head, pattern, weight):
+    topoList = ordinaTopo(graph, head)
+    pattern = "e" + pattern
+    righe = len(pattern)
+    colonne = len(topoList)
+    matrix = emptyMatrix(righe, colonne)
+    cost = -1000;
+    #nota: mat[0,0] = 0
+    for i in range(0, righe):
+        matrix[i][0] = i * weight
+
+    for c in range(1,colonne):
+        #trova il minimo predecessore del nodo
+        nodes = predecessore(graph,topoList[c])
+        cellsPreds=[]
+        for i in range(0,c):
+            if(topoList[i] in nodes):
+                cellsPreds.append(matrix[1][i])
+        matrix[1][c] = min(cellsPreds)
+
+    for i in range(1, righe):
+        for c in range(1, colonne):
+            #matrix[i,c] = computeCell(matrix,righe, colonne, graph, head, pattern, 4)
+            if pattern[i] == etichette[topoList[c]][0]:
+                #match
+                if(pattern[i] == 'A'):
+                    cost = 0
+                else:
+                    cost = 1
+            else:
+                cost = weight;
+            matrix[i][c] = calcolateMove(matrix, i, c, graph, topoList[c],topoList)
+
+    return matrix
+
+    
+
+
+def fixGraph(graph, head, etichette):
+    topolist = ordinaTopo(graph, head)
+    graph.update({"0":[topolist[0]]})
+    etichette.update({"0":["S0"]})
+    
+
 def main():
     graph = {
         "1" : ["2","3"],
         "2" : ["4"],
-        "3" : ["4"]
+        "3" : ["4"],
+        "4" : []
     }
     etichette={
         "1":["C"],
@@ -53,10 +140,13 @@ def main():
 
     pattern = "CAC"
     
-    print(ordinaTopo(graph, "1"))
+    fixGraph(graph, "1", etichette)
+    print(graph)
+    print(ordinaTopo(graph, "0"))
+    print(buildMatrix(graph, etichette, "0", pattern, 4))
 
 
-
+#RICORDAESI DI METTERE IL PREDECESSORE
 print("hello")
 main()
 
